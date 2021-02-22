aa24d9b0d449f3206f7efb5106c6768a7ef5eb1a
dauryellen <dauryellen@live.com>
2021-02-22 11:03:20 -0300

    criando as primeiras rotas

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,26 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express();
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Excluir
+ * PATCH => Alteração específica
+ */
+
+// ==> (GET) http://localhost:3333
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+// ==> (POST) http://localhost:3333
+
+app.post("/", (request, response) => {
+  // Recebeu os dados para salvar
+  return response.json({ message: "OS dados foram salvos com sucesso!" });
+});
 
 app.listen(3333, () => console.log("Server is running!"));
