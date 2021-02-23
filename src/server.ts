5a614307f5db6ceab679917afa3fcc45fb0376e7
dauryellen <dauryellen@live.com>
2021-02-23 10:45:53 -0300

    limpando o arquivo server

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
 import "reflect-metadata";
 import express, { request } from "express";
 import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Excluir
- * PATCH => Alteração específica
- */
-
-// ==> (GET) http://localhost:3333
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
-
-// ==> (POST) http://localhost:3333
-
-app.post("/", (request, response) => {
-  // Recebeu os dados para salvar
-  return response.json({ message: "OS dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
