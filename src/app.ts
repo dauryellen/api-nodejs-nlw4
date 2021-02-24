db9f0b13077f960f78eea499f792ccd0712f7d99
dauryellen <dauryellen@live.com>
2021-02-24 20:45:53 -0300

    refatorando a conexão com banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import "reflect-metadata";
-import "./database";
+import createConnection from "./database";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
