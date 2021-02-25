85a372b3b11e0178cb054e8a73942ec020e1ed0b
dauryellen <dauryellen@live.com>
2021-02-25 10:43:58 -0300

    criando o serviço de envio de e-mail

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
 
 class SendMailController {
   async execute(request: Request, response: Response) {
@@ -18,11 +19,11 @@ class SendMailController {
       return response.status(400).json({ error: "User does not exists!" });
     }
 
-    const surveyAlreadyExists = await surveysRepository.findOne({
+    const survey = await surveysRepository.findOne({
       id: survey_id,
     });
 
-    if (!surveyAlreadyExists) {
+    if (!survey) {
       return response.status(400).json({ error: "Survey does not exists!" });
     }
 
@@ -34,6 +35,8 @@ class SendMailController {
     await surveysUsersRepository.save(surveyUser);
 
     // Enviar e-mail para o usuário
+
+    await SendMailService.execute(email, survey.title, survey.description);
 
     return response.json(surveyUser);
   }
