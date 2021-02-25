7a91eed642be635b2c9247c8edefe8fdc9d80cb8
dauryellen <dauryellen@live.com>
2021-02-25 18:37:48 -0300

    criando relacionamento muitos para um

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -40,6 +40,7 @@ class SendMailController {
 
     const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
       where: [{ user_id: user.id }, { value: null }],
+      relations: ["user", "survey"],
     });
 
     if (surveyUserAlreadyExists) {
